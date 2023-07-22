b46262ff00cc5876724ca46e8b97c4e180c73ec4
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-22 18:10:57 -0400

    Update Solution.py

--- Problems/1 - 100/26. Remove Duplicates from Sorted Array/Solution.py
+++ Problems/1 - 100/26. Remove Duplicates from Sorted Array/Solution.py
@@ -6,9 +6,8 @@ class Solution(object):
         """
         x, current_duplicate_value = 1, nums[0]
         for i in range(1, len(nums)):
-            current_value = nums[i]
-            if current_value != current_duplicate_value:
-                nums[x] = current_duplicate_value = current_value
+            if nums[i] != current_duplicate_value:
+                nums[x] = current_duplicate_value = nums[i]
                 x+=1
 
         return x
