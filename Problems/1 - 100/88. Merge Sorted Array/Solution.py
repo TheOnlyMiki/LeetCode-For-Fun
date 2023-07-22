932e060aeee999348d22f7c074a5b1ed7b8e0b16
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-22 18:01:32 -0400

    Update Solution.py

--- Problems/1 - 100/88. Merge Sorted Array/Solution.py
+++ Problems/1 - 100/88. Merge Sorted Array/Solution.py
@@ -10,39 +10,20 @@ class Solution(object):
         #Initial positive for nums1 and nums2
         x, y = 0, 0
 
-        #Clone nums1 to the last 
-        if n != 0: 
-            if m != 0 and m > n: 
-                nums1[m:] = nums1[:m]
-            else:
-                #Clone nums1
-                clone_nums1 = nums1[:]
-                # Forloop in O(m+n)
-                for i in range(m + n):
-                    if x == m:
-                        nums1[i] = nums2[y]
-                        y+=1
-                    elif y == n:
-                        nums1[i] = clone_nums1[x]
-                        x+=1
-                    elif clone_nums1[x] >= nums2[y]:
-                        nums1[i] = nums2[y]
-                        y+=1
-                    elif clone_nums1[x] < nums2[y]:
-                        nums1[i] = clone_nums1[x]
-                        x+=1
-                return
-            
-            for i in range(m + n):
-                if x == m:
-                    nums1[i] = nums2[y]
-                    y+=1
-                elif y == n:
-                    nums1[i] = nums1[m+x]
-                    x+=1
-                elif nums1[m+x] >= nums2[y]:
-                    nums1[i] = nums2[y]
-                    y+=1
-                elif nums1[m+x] < nums2[y]:
-                    nums1[i] = nums1[m+x]
-                    x+=1
+        #Clone nums1
+        clone_nums1 = nums1[:m]
+
+        # Forloop in O(m+n)
+        for i in range(m + n):
+            if x == m:
+                nums1[i] = nums2[y]
+                y+=1
+            elif y == n:
+                nums1[i] = clone_nums1[x]
+                x+=1
+            elif clone_nums1[x] >= nums2[y]:
+                nums1[i] = nums2[y]
+                y+=1
+            elif clone_nums1[x] < nums2[y]:
+                nums1[i] = clone_nums1[x]
+                x+=1
