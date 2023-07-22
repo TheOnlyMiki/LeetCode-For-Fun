8af02bb638333ba59debc22f15f9c03fb26fc2d2
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-22 18:27:32 -0400

    Update Solution.py

--- Problems/1 - 100/80. Remove Duplicates from Sorted Array II/Solution.py
+++ Problems/1 - 100/80. Remove Duplicates from Sorted Array II/Solution.py
@@ -6,16 +6,16 @@ class Solution(object):
         """
 
         x = 0
-        duplicate = 0 
+        duplicate = 0
 
         for value in nums:
             print(value, nums[x-1], duplicate)
-            if value == nums[x-1] and duplicate < 2:
-                duplicate+=1
+            if value != nums[x-1]:
+                duplicate = 1
                 nums[x] = value
                 x+=1
-            elif value != nums[x-1]:
-                duplicate = 1
+            elif duplicate < 2:
+                duplicate+=1
                 nums[x] = value
                 x+=1
 
