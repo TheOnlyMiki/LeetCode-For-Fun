d3beabee8c38e76b19985cb5d1970cd3e9ca2498
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-22 18:23:06 -0400

    Update Solution.py

--- Problems/1 - 100/26. Remove Duplicates from Sorted Array/Solution.py
+++ Problems/1 - 100/26. Remove Duplicates from Sorted Array/Solution.py
@@ -4,6 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #Option 2 - Less code, but cost O(n) + O(nlogn) + O(n) -> nums[:] + sorted + set()
+        nums[:] = sorted(set(nums))
+        return len(nums)
+
+        # Option 1 - This usually less operations than Option 1
+        """
         x, current_duplicate_value = 1, nums[0]
         for i in range(1, len(nums)):
             if nums[i] != current_duplicate_value:
@@ -11,3 +17,4 @@ class Solution(object):
                 x+=1
 
         return x
+        """
