a32bc2c880705e0d047f6b483363ac949735a93e
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-22 18:00:11 -0400

    Update Solution.py

--- Problems/1 - 100/88. Merge Sorted Array/Solution.py
+++ Problems/1 - 100/88. Merge Sorted Array/Solution.py
@@ -10,19 +10,20 @@ class Solution(object):
         #Initial positive for nums1 and nums2
         x, y = 0, 0
 
-        #Clone nums1
-        clone_nums1 = nums1[:]
+        #Clone nums1 to the last 
+        if n != 0: 
+            if m != 0: nums1[m:] = nums1[:m]
 
-        for i in range(m + n):
-            if x == m:
-                nums1[i] = nums2[y]
-                y+=1
-            elif y == n:
-                nums1[i] = clone_nums1[x]
-                x+=1
-            elif clone_nums1[x] >= nums2[y]:
-                nums1[i] = nums2[y]
-                y+=1
-            elif clone_nums1[x] < nums2[y]:
-                nums1[i] = clone_nums1[x]
-                x+=1
+            for i in range(m + n):
+                if x == m:
+                    nums1[i] = nums2[y]
+                    y+=1
+                elif y == n:
+                    nums1[i] = nums1[m+x]
+                    x+=1
+                elif nums1[m+x] >= nums2[y]:
+                    nums1[i] = nums2[y]
+                    y+=1
+                elif nums1[m+x] < nums2[y]:
+                    nums1[i] = nums1[m+x]
+                    x+=1
