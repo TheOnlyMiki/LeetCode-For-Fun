90d7fadf635917ee3c4d4cdba35ed2c395135c6b
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-22 18:39:05 -0400

    Create Solution.py

---
+++ Problems/500-599/560. Subarray Sum Equals K/Solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        sums = {}
+        consum = 0
+        output = 0
+
+        for value in nums:
+            if consum not in sums:
+                sums[consum] = 1
+            else:
+                sums[consum] += 1
+
+            consum += value
+
+            if consum - k in sums:
+                output += sums[consum - k]
+            
+        return output
