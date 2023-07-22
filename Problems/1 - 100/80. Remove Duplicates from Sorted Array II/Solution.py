ef03f40c37bb2fb82e1dc6416f4e67c58c33836a
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-22 18:29:42 -0400

    Update Solution.py

--- Problems/1 - 100/80. Remove Duplicates from Sorted Array II/Solution.py
+++ Problems/1 - 100/80. Remove Duplicates from Sorted Array II/Solution.py
@@ -4,19 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
+        # Option 2
         x = 0
         duplicate = 0
 
         for value in nums:
-            print(value, nums[x-1], duplicate)
-            if value != nums[x-1]:
-                duplicate = 1
-                nums[x] = value
-                x+=1
-            elif duplicate < 2:
+            if duplicate < 2 or value != nums[x-1]:
+                if value != nums[x-1]:
+                    duplicate = 0
                 duplicate+=1
                 nums[x] = value
                 x+=1
 
         return x
+        
+        # Option 1
+        """
+        x = 1
+        duplicate_value = nums[0]
+        duplicate_allow = True
+
+        for i in range(1, len(nums)):
+            if duplicate_value != nums[i]:
+                nums[x] = duplicate_value = nums[i]
+                duplicate_allow = True
+                x+=1
+            elif duplicate_allow:
+                nums[x] = duplicate_value
+                duplicate_allow = False
+                x+=1
+
+        return x
+        """
