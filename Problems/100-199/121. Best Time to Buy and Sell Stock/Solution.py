4688d21f2078ef31ae953e0210255e7d084a9a45
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-23 19:38:32 -0400

    Create Solution.py

---
+++ Problems/100-199/121. Best Time to Buy and Sell Stock/Solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        pre_low = prices[0]
+        for price in prices:
+            if pre_low > price:
+                pre_low = price
+                continue
+
+            current_profit = price - pre_low
+            
+            if profit < current_profit:
+                profit = current_profit
+
+        return profit
