69081384b1b862e6fc469809f51d41a8e31eacf2
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-23 19:47:31 -0400

    Update Solution.py

--- Problems/1 - 99/55. Jump Game/Solution.py
+++ Problems/1 - 99/55. Jump Game/Solution.py
@@ -5,8 +5,20 @@ class Solution(object):
         :rtype: bool
         """
 
-        last_index = len(nums) - 1
-        if sum(nums[1:-1]) > last_index:
+        if len(nums) == 1:
             return True
 
-        return False
+        return self.checkReachable(nums, len(nums)-2, 1)
+        
+    def checkReachable(self, nums, position, distance):
+        print(distance, position, nums[position])
+        if position == 0:
+            if nums[position] < distance:
+                return False
+            else:
+                return True
+        
+        if nums[position] < distance:
+            return self.checkReachable(nums, position-1, distance+1)
+        else:
+            return self.checkReachable(nums, position-1, 1)
