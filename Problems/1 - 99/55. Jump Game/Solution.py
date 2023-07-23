e748aded21376cf47b49645a33fab46524e03aad
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-23 19:48:06 -0400

    Update Solution.py

--- Problems/1 - 99/55. Jump Game/Solution.py
+++ Problems/1 - 99/55. Jump Game/Solution.py
@@ -4,12 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        last_index = len(nums) - 1
 
-        if len(nums) == 1:
+        prejump = {0:0}
+        for i, jump in enumerate(nums[:last_index]):
+            if i + jump in prejump:
+                prejump[i + jump].append(i)
+            else:
+                prejump[i + jump] = [i]
+
+        print(prejump)  
+
+        if max(prejump.keys()) >= last_index:
             return True
+        
+        return False    
 
+
+"""
+#Recursion backward method - slow and one step by step check
         return self.checkReachable(nums, len(nums)-2, 1)
         
+
     def checkReachable(self, nums, position, distance):
         print(distance, position, nums[position])
         if position == 0:
@@ -22,3 +38,4 @@ class Solution(object):
             return self.checkReachable(nums, position-1, distance+1)
         else:
             return self.checkReachable(nums, position-1, 1)
+"""
