e9ccf4cce0b867ca155d41f652b18200732a7ce0
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-23 21:11:14 -0400

    Create Solution.py

---
+++ Problems/200-299/205. Isomorphic Strings/Solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) != len(t):
+            return False
+
+        public = {'s':{}, 't': {}}
+        for i in range(len(s)):
+            print(public)
+            if public['s'].get(s[i]) != public['t'].get(t[i]):
+                return False
+            public['s'][s[i]] = i
+            public['t'][t[i]] = i
+
+        return True
