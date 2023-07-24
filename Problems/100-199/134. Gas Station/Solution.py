935d3b46afb7279acf4cf95886426a9a02ada69c
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-24 16:07:13 -0400

    Update Solution.py

--- Problems/100-199/134. Gas Station/Solution.py
+++ Problems/100-199/134. Gas Station/Solution.py
@@ -10,7 +10,7 @@ class Solution(object):
         remain_gas = 0
         start = 0
 
-        if sum(gas) - sum(cost) < 0:
+        if sum(gas) < sum(cost):
             return -1
 
         for i, spend in enumerate(cost):
