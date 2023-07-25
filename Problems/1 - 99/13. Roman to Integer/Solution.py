686288bb4bca12209a85b473b73501601f10b3f1
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-25 12:50:17 -0400

    Update Solution.py

--- Problems/1 - 99/13. Roman to Integer/Solution.py
+++ Problems/1 - 99/13. Roman to Integer/Solution.py
@@ -13,22 +13,30 @@ class Solution(object):
                     'D' : 500,
                     'M' : 1000 }
 
-        roman_special = {   "IV" : roman['I'] + roman['V'] - 4,
-                            "IX" : roman['I'] + roman['X'] - 9,
-                            "XL" : roman['X'] + roman['L'] - 40,
-                            "XC" : roman['X'] + roman['C'] - 90,
-                            "CD" : roman['C'] + roman['D'] - 400,
-                            "CM" : roman['C'] + roman['M'] - 900  }
+        roman_special = {   "IV" : 2,
+                            "IX" : 2,
+                            "XL" : 20,
+                            "XC" : 20,
+                            "CD" : 200,
+                            "CM" : 200  }
+
+        # Pso code
+        """
+        roman_special = {   "IV" : roman['I'] + roman['V'] - 4,     #1+5-4=2
+                            "IX" : roman['I'] + roman['X'] - 9,     #1+10-9=2
+                            "XL" : roman['X'] + roman['L'] - 40,    #10+50-40=20
+                            "XC" : roman['X'] + roman['C'] - 90,    #10+100-90=20
+                            "CD" : roman['C'] + roman['D'] - 400,   #100+500-400=200
+                            "CM" : roman['C'] + roman['M'] - 900  } #100+1000-900=200
+        """
 
         num = 0
-        i = 0
-        refund = 0
 
         for special in roman_special:
             if special in s:
-                refund += roman_special[special]
+                num -= roman_special[special]
 
-        for i, c in enumerate(s):
+        for c in s:
             num += roman[c]
 
-        return num - refund
+        return num
