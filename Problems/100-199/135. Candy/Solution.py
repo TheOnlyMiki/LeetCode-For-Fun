67856e88eba19cea111da961f40dd6f30ef85738
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-25 12:37:43 -0400

    Update Solution.py

--- Problems/100-199/135. Candy/Solution.py
+++ Problems/100-199/135. Candy/Solution.py
@@ -5,33 +5,14 @@ class Solution(object):
         :rtype: int
         """
         n = len(ratings)
-        #count = [1] * n
-        count1 = {}
+        count = [1] * n
+        
+        for i in range(1, n):
+            if ratings[i] > ratings[i-1]:
+                count[i] = count[i-1] + 1
+        
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                count[i] = max(count[i], count[i+1] + 1)
 
-        for i, value in enumerate(ratings):
-            if value in count1:
-                count1[value].append(i)
-            else:
-                count1[value] = [i]
-
-        lowest = min(count1.keys())
-        low_list = count1[lowest]
-        count_candy = []
-
-        for low_index in low_list:
-            count = [1] * n
-            for i in range(low_index + 1, n):
-                if ratings[i] > ratings[i - 1]:
-                    count[i] = count[i - 1] + 1
-                else:
-                    count[i] = count[i - 1] - 1
-
-            for i in range(low_index - 1, -1 ,-1):
-                if ratings[i] > ratings[i + 1]:
-                    count[i] = count[i + 1] + 1
-                else:
-                    count[i] = count[i + 1] - 1
-
-            count_candy.append(sum(count))
-
-        return min(count_candy)
+        return sum(count)
