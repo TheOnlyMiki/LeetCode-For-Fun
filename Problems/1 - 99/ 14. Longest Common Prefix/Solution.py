31974cab7f98e529f95078b001bda987015a4698
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-26 13:45:20 -0400

    Update Solution.py

--- Problems/1 - 99/ 14. Longest Common Prefix/Solution.py
+++ Problems/1 - 99/ 14. Longest Common Prefix/Solution.py
@@ -4,17 +4,35 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        # Option 2
+        first_s_length = len(strs[0])
 
-        if min(strs) == 0:
-            return ""
+        i = 0
+        record = first_s_length
+        range_remain_s = range(1, len(strs))
 
+        while i < first_s_length:
+            c = strs[0][i]
+
+            for index in range_remain_s:
+                if i == len(strs[index]) or strs[index][i] != c:
+                    record = i
+                    i = first_s_length
+                    break
+
+            i += 1
+
+        return strs[0][:record]
+
+        #Option 1
+        """
         index = 200
         first_s_length = len(strs[0])
 
-        for s in strs[1:]:
+        for s in range(1,len(strs)):
             match = 0
 
-            for i, c in enumerate(s):
+            for i, c in enumerate(strs[s]):
                 if i < first_s_length and strs[0][i] == c:
                     match+=1
                 else:
@@ -23,3 +41,4 @@ class Solution(object):
             index = min(match, index)
 
         return strs[0][:index]
+        """
