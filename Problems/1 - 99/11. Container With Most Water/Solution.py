ae0e5c83b827702b894872aa8b1555e89f55b539
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-28 01:57:59 -0400

    Update Solution.py

--- Problems/1 - 99/11. Container With Most Water/Solution.py
+++ Problems/1 - 99/11. Container With Most Water/Solution.py
@@ -5,20 +5,21 @@ class Solution(object):
         :rtype: int
         """
 
-        distance = len(height)
+        distance = len(height) - 1
         left = 0
-        right = distance-1
+        right = distance
         max_record = 0
 
         while left != right:
-            distance -=1
-
-            max_record = max(max_record, distance * min(height[left], height[right]))
+            area = distance * min(height[left], height[right])
+            if max_record < area:
+                max_record = area
 
             # Initial next iteration
             if height[left] > height[right]:
                 right -= 1
             else:
                 left += 1
+            distance -=1
 
         return max_record
