70ec8d952ce7956faeb81e0b23d4d78e64fbb23a
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-07-28 18:57:47 -0400

    Update Solution.py

--- Problems/1 - 99/30. Substring with Concatenation of All Words/Solution.py
+++ Problems/1 - 99/30. Substring with Concatenation of All Words/Solution.py
@@ -5,12 +5,8 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
-        words_len = len(words)
         word_len = len(words[0])
-        subarray_len = words_len * word_len
-
-        if len(s) < subarray_len:
-            return []
+        subarray_len = len(words) * word_len
 
         count = {}
         for word in words:
@@ -20,29 +16,20 @@ class Solution(object):
                 count[word] = 1
 
         output = []
-        i, i_2 = 0, subarray_len-1
-        current_subarray = {}
+        i = 0
 
-        while i_2 < len(s):
+        while i < len(s) - subarray_len + 1:
+            temp_count = {}
             for index in range(i, i + subarray_len, word_len):
                 subarray = s[ index : index + word_len ]
-                if subarray in current_subarray:
-                    current_subarray[subarray] += 1
+                if subarray in temp_count:
+                    temp_count[subarray] += 1
                 else:
-                    current_subarray[subarray] = 1
+                    temp_count[subarray] = 1
 
-            match = 0
-            for word, value in count.items():
-                if word in current_subarray and current_subarray[word] == value:
-                    match += 1
-                else:
-                    break
-
-            if match == len(count):
+            if temp_count == count:
                 output.append(i)
 
-            current_subarray.clear()
             i += 1
-            i_2 += 1
 
         return output
