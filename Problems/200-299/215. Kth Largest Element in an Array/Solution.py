58eca90aa505985883616a6e7c33495d56490b2a
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-08-17 19:05:45 -0400

    Update Solution.py

--- Problems/200-299/215. Kth Largest Element in an Array/Solution.py
+++ Problems/200-299/215. Kth Largest Element in an Array/Solution.py
@@ -6,6 +6,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # Option 2 - Heap in k size
+        record = nums[:k]
+        heapq.heapify(record)
+
+        for i in xrange(k, len(nums)):
+            if nums[i] > record[0]:
+                heapq.heappushpop(record, nums[i])
+        
+        return record[0]
+
+        # Option 1 - Heap whole list
         nums = [-num for num in nums]
 
         heapq.heapify(nums)
