1f7e4d089a1309b3b27192e6c213060c460e8d12
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-09-06 01:43:45 -0400

    Update Solution.py

--- Problems/0100 - 0199/167. Two Sum II - Input Array Is Sorted/Solution.py
+++ Problems/0100 - 0199/167. Two Sum II - Input Array Is Sorted/Solution.py
@@ -5,7 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # Option 2 - Space O(1)
+        left, right = 0, len(numbers)-1
+        temp = None
+        while left < right:
+            temp = numbers[left] + numbers[right]
+            if temp < target:
+                left += 1
+            elif temp > target:
+                right -= 1
+            else:
+                return [left+1, right+1]
 
+        # Option 1 - Space O(n)
+        """
         diff = {}
         
         for i, number in enumerate(numbers):
@@ -14,3 +27,4 @@ class Solution(object):
             diff[target - number] = i+1
 
         return None
+        """
