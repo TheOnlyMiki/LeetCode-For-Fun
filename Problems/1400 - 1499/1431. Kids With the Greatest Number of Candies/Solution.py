1bb9b0f5ce1928a9a3be6427a84b999a6f98e0f0
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-09-11 18:54:23 -0400

    Update Solution.py

--- Problems/1400 - 1499/1431. Kids With the Greatest Number of Candies/Solution.py
+++ Problems/1400 - 1499/1431. Kids With the Greatest Number of Candies/Solution.py
@@ -5,7 +5,6 @@ class Solution(object):
         :type extraCandies: int
         :rtype: List[bool]
         """
-        output = []
         maximum = max(candies) - extraCandies
         for i in range(len(candies)):
             candies[i] = False if candies[i] < maximum else True
