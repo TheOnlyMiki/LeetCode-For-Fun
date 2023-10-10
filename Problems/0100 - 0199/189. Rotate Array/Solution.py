40ac005395449af5b5ee618328bcd4eafea107ad
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-10-10 17:21:26 -0400

    Update Solution.py

--- Problems/0100 - 0199/189. Rotate Array/Solution.py
+++ Problems/0100 - 0199/189. Rotate Array/Solution.py
@@ -5,11 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        # Option 2 - Space O(1)
+        k = k % len(nums)
 
-        #n = len(nums)
-        #k_new = k % n
-        shift_position = k % len(nums)
+        def swap(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left, right = left + 1, right - 1
 
-        #return nums[-shift_position:] + nums[:-shift_position]
+        swap(0, len(nums)-1-k)
+        swap(len(nums)-k, len(nums)-1)
+        swap(0, len(nums)-1)
+        
+        # Option 1 - Space O(n)
+        """
+        shift_position = len(nums) - (k % len(nums))
 
-        nums[:] = nums[-shift_position:] + nums[:-shift_position]
+        #return nums[shift_position:] + nums[:shift_position]
+
+        nums[:] = nums[shift_position:] + nums[:shift_position]
+        """
