227c6a40a81aa8a4e071514e2c75ae2291d8e08c
JingZeng Xie <60678561+TheOnlyMiki@users.noreply.github.com>
2023-10-11 13:01:32 -0400

    Create Solution.py

---
+++ Problems/0300 - 0399/350. Intersection of Two Arrays II/Solution.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        # Option 2 - Hash Table
+        """
+        record = {}
+        for num in nums1:
+            record[num] = record[num] + 1 if num in record else 1
+
+        output = []
+        for num in nums2:
+            if num in record and record[num] != 0:
+                output.append(num)
+                record[num] -= 1
+
+        return output
+        """
+
+        # Option 1 - Sorting Method
+        nums1.sort()
+        nums2.sort()
+        x, y = len(nums1), len(nums2)
+        output = []
+        i, j = 0, 0
+        while i < x and j < y:
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                output.append(nums1[i])
+                i, j = i+1, j+1
+
+        return output
